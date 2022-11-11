a4ed2675bfe78c71329b851772bd22a2671f25c8
Anirban Kar <thecodacus@gmail.com>
2022-11-11 19:25:13 +0000

    debug

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 "use strict";
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const serverless = require("serverless-http");
@@ -16,42 +16,42 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const router = express.Router();
 
-// router.get('/', (req, res) => {
-//   res.writeHead(200, { 'Content-Type': 'text/html' });
-//   res.write('<h1>Hello from Express.js!</h1>');
-//   res.end();
-// });
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
 // router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post("/", (req, res) => res.json({ postBody: req.body }));
 
 router.get("/instructions-module/:module", async (req, res) => {
-  try {
-    const { module } = req.params;
-    const filePath = path.resolve(`public/${module}.md`);
-    fs.readFile(filePath, function (e, data) {
-      if (e) {
-        console.error(e);
-        if (e.message?.includes("no such file or directory")) {
-          res.status(404).json({ message: "no such file or directory" });
-          return;
-        }
-        console.error(e);
-        res.status(500).json({ error: e.message });
-        return;
-      }
-      res.status(200).json(data.toString());
-    });
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ error: e.message });
-  }
+	try {
+		const { module } = req.params;
+		const filePath = path.resolve(`./public/${module}.md`);
+		fs.readFile(filePath, function (e, data) {
+			if (e) {
+				console.error(e);
+				if (e.message?.includes("no such file or directory")) {
+					res.status(404).json({ message: "no such file or directory" });
+					return;
+				}
+				console.error(e);
+				res.status(500).json({ error: e.message });
+				return;
+			}
+			res.status(200).json(data.toString());
+		});
+	} catch (e) {
+		console.error(e);
+		res.status(500).json({ error: e.message });
+	}
 });
 
-router.get('/', greetingsController);
-router.get('/student-data', fetchStudentDataFromGoogleSheetController)
+router.get("/", greetingsController);
+router.get("/student-data", fetchStudentDataFromGoogleSheetController);
 
 app.use("/.netlify/functions/server", router); // path must route to lambda
-app.use("/", (req, res) => res.sendFile(path.join(__dirname, "../index.html")));
+app.use("/", (req, res) => res.sendFile(path.join("index.html")));
 
 module.exports = app;
 module.exports.handler = serverless(app);
